d9b74b998f5f85ab50430cd6b62f781b491d6f49
ashish1508 <belide.ashish@gmail.com>
2021-05-26 00:40:07 +0530

    Update rainWaterTrapped.cpp

--- stack/rainWaterTrapped.cpp
+++ stack/rainWaterTrapped.cpp
@@ -2,14 +2,15 @@ int Solution::trap(const vector<int> &A) {
     if(A.size()<3) return 0;
     int i=0,j=A.size()-1;
     int ans=0,lmax=A[i],rmax=A[j];
+    
     while(i<=j){
+        lmax = max(lmax,A[i]);
+        rmax = max(rmax,A[j]);
         if(lmax<rmax){
             ans += lmax-A[i];
-            lmax = max(lmax,A[i+1]);
             i++;
         }else{
             ans += rmax-A[j];
-            rmax = max(rmax,A[j-1]);
             j--;
         }
     }

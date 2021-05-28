a24898411b728eda3e91ba21503cbfadbcff509a
ashish1508 <belide.ashish@gmail.com>
2021-05-29 00:46:45 +0530

    Update 567_permutationInString.cpp

--- leetcode/slidingWindow/567_permutationInString.cpp
+++ leetcode/slidingWindow/567_permutationInString.cpp
@@ -9,21 +9,20 @@ public:
     bool checkInclusion(string s1, string s2) {
         if(s1.size()>s2.size()) return false;
         vector<int> c(26,0);
-        unordered_set<char> us;
+
         for(int i=0;i<s1.size();i++){
             c[s1[i]-97]++;
-            us.insert(s1[i]);
         }
         
         int i=0,j=0,ans=0, n=s2.size();
         for(j=0;j<s1.size();j++){
-            if(us.find(s2[j])!=us.end()) c[s2[j]-97]--;
+             c[s2[j]-97]--;
         }
         if(checkAllZeros(c)) return true;
         
         while(j<n){
-            if(us.find(s2[j])!=us.end()) c[s2[j]-97]--;
-            if(us.find(s2[i])!=us.end()) c[s2[i]-97]++;
+            c[s2[j]-97]--;
+            c[s2[i]-97]++;
             if(checkAllZeros(c)) return true;
             j++;
             i++;   

ccaca9d6d103e41e36ef5bbef740458f3b831cd2
ashish1508 <belide.ashish@gmail.com>
2021-05-28 19:46:47 +0530

    Create 424_longestRepeatingCharacterReplacement.cpp

---
+++ leetcode/slidingWindow/424_longestRepeatingCharacterReplacement.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> v(26,0);
+        int i=0,j=0,max_count=0,ans=0;
+        while(j<s.size()){
+            max_count = max(max_count,++v[s[j]-65]);
+            if(j-i+1-max_count>k){
+                v[s[i]-65]--;
+                i++;
+            }
+            ans = max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};

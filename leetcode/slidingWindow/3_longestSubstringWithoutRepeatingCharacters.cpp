f09583b9e659529205722f665fe1e30da2503a86
ashish1508 <belide.ashish@gmail.com>
2021-05-29 12:00:35 +0530

    Create 3_longestSubstringWithoutRepeatingCharacters.cpp

---
+++ leetcode/slidingWindow/3_longestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int onesAndZeroes(vector<int> &v){
+        for(int i=0;i<v.size();i++){
+            if(v[i]!=0 && v[i]!=1) return 0;
+        }
+        return 1;
+    }
+    int lengthOfLongestSubstring(string s) {
+       vector<int> v(128,0);
+        int i=0,j=0, n=s.size(),ans=0;
+        while(j<n){
+            v[s[j]-'\0']++;
+            
+            while(!onesAndZeroes(v) && i<j){
+                v[s[i]-'\0']--;
+                i++;
+            }
+            ans = max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};

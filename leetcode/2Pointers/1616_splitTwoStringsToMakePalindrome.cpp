153f7ba5daba2aec649097743ae8c4182bc58d0e
ashish1508 <belide.ashish@gmail.com>
2021-06-07 23:13:26 +0530

    Create 1616_splitTwoStringsToMakePalindrome.cpp

---
+++ leetcode/2Pointers/1616_splitTwoStringsToMakePalindrome.cpp
@@ -0,0 +1,21 @@
+// discuss solution 
+// for better understanding : https://www.youtube.com/watch?v=VCF8_5dELaU&ab_channel=NareshGupta
+class Solution {
+public:
+    bool help(string a,string b){
+        int n=b.size(),i=0,j=n-1;
+        while(i<j && a[i]==b[j]) i++,j--;
+        if(i>=j) return true;
+        int tmpi=i,tmpj=j;
+        while(i<j && b[i]==b[j]) i++,j--;
+        if(i>=j) return true;
+        i=tmpi,j=tmpj;
+        while(i<j && a[i]==a[j]) i++,j--;
+        return i>=j;
+    }
+
+    bool checkPalindromeFormation(string a, string b)
+    {
+        return help(a,b)||help(b,a);
+    }
+};

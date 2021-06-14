f514cb0befef53baf0454e9eb15030264b2b2ea8
ashish1508 <belide.ashish@gmail.com>
2021-06-14 18:16:11 +0530

    Update 81_searchInRotatedSortedArrayII.cpp

--- leetcode/binarysearch/81_searchInRotatedSortedArrayII.cpp
+++ leetcode/binarysearch/81_searchInRotatedSortedArrayII.cpp
@@ -1,4 +1,23 @@
-// comapring with low
+// my implementation
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo=0,n=nums.size(),hi=n-1,mid;
+        while(lo<=hi){
+            cout<<lo<<" "<<hi<<endl;
+            mid=lo+(hi-lo)/2;
+            if(nums[mid]==target) return mid;
+            if((nums[mid]>nums[hi] && target<=nums[hi])  || (nums[mid]>nums[hi] && target>nums[mid]) || (nums[mid]<nums[hi] && target>nums[mid] && target<=nums[hi] )) lo=mid+1;
+            else if(nums[mid]==nums[hi]) hi--;
+            else hi=mid-1;
+        }
+        return -1;
+    }
+};
+
+
+// below solutions use the fact that part below or after the mid remains sorted
+// comparing with low
 class Solution {
 public:
     int search(vector<int>& nums, int target) {

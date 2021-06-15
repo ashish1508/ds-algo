9698789b3defdf78ff575d196573bc65daf60b6b
ashish1508 <belide.ashish@gmail.com>
2021-06-15 15:01:38 +0530

    Create 240_searchA2DMatrixII.cpp

---
+++ leetcode/binarysearch/240_searchA2DMatrixII.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r=matrix.size(),c=matrix[0].size(),lo=0,hi=c-1,mid,i=0;
+        while(i<r){
+            lo=0;
+            while(lo<hi){
+                mid=lo+(hi-lo)/2;
+                if(matrix[i][mid]<target) lo=mid+1;
+                else hi=mid;
+            }
+            if(matrix[i][lo]==target) return true;
+            i++;
+        }
+        return false;
+    }
+};

c813db42c08c0b13086b13c5207b0e5465122e5e
ashish1508 <belide.ashish@gmail.com>
2021-06-25 00:44:35 +0530

    Create 17_letterCombinationsOfAPhoneNumber.cpp

---
+++ leetcode/backtracking/17_letterCombinationsOfAPhoneNumber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void getCombinations(int s, string &path, vector<string> &vs, string &digits, vector<string> &dialpad){
+        if(path.size()==digits.size()){
+            vs.push_back(path);
+            return;
+        }
+        for(int i=0;i<dialpad[digits[s]-'2'].size();i++){
+            path.append(1,dialpad[digits[s]-'2'][i]);
+            getCombinations(s+1,path,vs,digits,dialpad);
+            path.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0) return {};
+        vector<string> dialpad = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string> vs;
+        string s="";
+        getCombinations(0,s,vs,digits,dialpad);
+        return vs;
+    }
+};

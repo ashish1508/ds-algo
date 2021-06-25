a7cf572e60d4098f026e2dbb8dd0db502f8d1790
ashish1508 <belide.ashish@gmail.com>
2021-06-25 15:54:22 +0530

    Update 47_permutationsII.cpp

--- leetcode/backtracking/47_permutationsII.cpp
+++ leetcode/backtracking/47_permutationsII.cpp
@@ -1,4 +1,35 @@
 
+
+// like normal permutations with unordered_set to maintain unique numbers
+class Solution {
+public:
+    void getPermutations(vector<int> &nums, vector<int> &path, int s, vector<vector<int>> &ans){
+        if(path.size()==nums.size()){
+            ans.push_back(path);
+            return;
+        }
+        unordered_set<int> us;
+        for(int i=s;i<nums.size();i++){
+            if(us.find(nums[i])!=us.end()) continue;
+            us.insert(nums[i]);
+            swap(nums[i],nums[s]);
+            path.push_back(nums[s]);
+            getPermutations(nums,path,s+1,ans);
+            path.pop_back();
+            swap(nums[i],nums[s]);
+            
+        }
+        return;
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        getPermutations(nums,path,0,ans);
+        return ans;
+    }
+};
+
+
 // keep track of all unique numbers at every position
 class Solution {
 public:

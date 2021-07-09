63f96545a8d3bfa9cf98bbcd51e6098bd1f5c478
ashish1508 <belide.ashish@gmail.com>
2021-07-10 02:50:04 +0530

    Update evaluateExpressiionToTrue.cpp

--- dp/evaluateExpressiionToTrue.cpp
+++ dp/evaluateExpressiionToTrue.cpp
@@ -1,5 +1,7 @@
 
 // approach : take boolean at last
+// how to apply mod seen from comments gfg
+// top down
 int mod=1003; 
 pair<int,int> go(int s, int e, string &st, vector<vector<pair<int,int>>> &cache){
     if(s>e) return {0,0};
@@ -32,3 +34,40 @@ int Solution::cnttrue(string A) {
     pair<int,int> p = go(0,A.size()-1,A,cache);
     return p.first;
 }
+
+
+// bottom up
+int mod=1003
+int Solution::cnttrue(string A) {
+    int n = A.size();
+    vector<vector<pair<int,int>>> cache;
+    cache.resize(A.size(),vector<pair<int,int>>(A.size(),{-1,-1}));
+    for(int i=0;i<n;i+=2){
+        if(A[i]=='T') cache[i][i] = {1,0};
+        else cache[i][i] = {0,1};
+    }
+    for(int gap=2;gap<n;gap+=2){
+        for(int i=0;i<n-gap;i++){
+            int j=i+gap;
+            int nt=0,nf=0;
+            for(int k=i+1;k<j;k+=2){
+                pair<int,int> before = cache[i][k-1];
+                pair<int,int> after =  cache[k+1][j];
+                if(A[k]=='&'){
+                    nt += (before.first * after.first);
+                    nf += before.first * after.second + before.second * after.first + before.second * after.second;
+                }else if(A[k]=='|'){
+                    nt += before.first * after.first + before.first * after.second + before.second * after.first;
+                    nf += before.second * after.second;
+                }else if(A[k]=='^'){
+                    nt += before.first * after.second + before.second * after.first;
+                    nf += before.first * after.first + before.second * after.second;
+                }
+            }
+            cache[i][j] = {nt%mod,nf%mod};
+        }
+    }
+    pair<int,int> p = cache[0][A.size()-1];
+    return p.first;
+}
+
